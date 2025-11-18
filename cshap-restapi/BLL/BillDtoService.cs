b963364f74b141d09c6658e7566b6fbd842714f1
Youfang yao <yyfsingler@163.com>
2025-11-17 20:54:48 -0700

    add service for BillDto

---
+++ cshap-restapi/BLL/BillDtoService.cs
@@ -0,0 +1,42 @@
+﻿using CShap.RestApi.DAL;
+using cshap_restapi.BLL.Interfaces;
+using cshap_restapi.ViewModels;
+
+namespace cshap_restapi.BLL
+{
+    public class BillDtoService : IBillDtoService
+    {
+        private readonly AppDbContext _context;
+
+        public BillDtoService(AppDbContext context)
+        {
+            if (context == null) throw new ArgumentNullException("context is null");
+            _context = context;
+        }
+
+        public async Task<SResult<List<BillDto>>> GetAllAsync()
+        {
+            return SResult<List<BillDto>>.Fail("No implement");
+        }
+
+        public async Task<SResult<BillDto>> GetByIdAsync(int id)
+        {
+            return SResult<BillDto>.Fail("No implement");
+        }
+
+        public async Task<SResult<BillDto>> CreateAsync(BillDto dto)
+        {
+            return SResult<BillDto>.Fail("No implement");
+        }
+
+        public async Task<SResult> UpdateAsync(int id, BillDto dto)
+        {
+            return SResult.Fail("No implement");
+        }
+
+        public async Task<SResult> DeleteAsync(int id)
+        {
+            return SResult.Fail("No implement");
+        }
+    }
+}
---
+++ cshap-restapi/BLL/Interfaces/IBillDtoService.cs
@@ -0,0 +1,17 @@
+﻿using cshap_restapi.ViewModels;
+
+namespace cshap_restapi.BLL.Interfaces
+{
+    public interface IBillDtoService
+    {
+        Task<SResult<List<BillDto>>> GetAllAsync();
+
+        Task<SResult<BillDto>> GetByIdAsync(int id);
+
+        Task<SResult<BillDto>> CreateAsync(BillDto dto);
+
+        Task<SResult> UpdateAsync(int id, BillDto dto);
+
+        Task<SResult> DeleteAsync(int id);
+    }
+}

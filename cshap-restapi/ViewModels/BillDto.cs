ee84707708380718e136380b6d7d98eb76026874
Youfang yao <yyfsingler@163.com>
2025-11-17 20:35:36 -0700

    add BillDto view model

---
+++ cshap-restapi/ViewModels/BillDto.cs
@@ -0,0 +1,15 @@
+﻿using CShap.RestApi.Entities;
+using System.Runtime.InteropServices;
+
+namespace cshap_restapi.ViewModels
+{
+    public class BillDto
+    {
+        public int BillID { get; set; }
+        public string PayeeName { get; set; } = string.Empty;
+        public double PaymentDue {  get; set; }
+        public bool Paid { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}

61ab3c0341c115b0ace00001ec876a2825c8d62c
Hansenyao <yyfsingler@163.com>
2025-11-22 20:10:08 -0700

    add BillDialog for Add operation

---
+++ react-restclient/components/BillDialog.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { Checkbox, FormControlLabel } from "@mui/material";
+import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button } from "@mui/material";
+
+export default function BillDialog({ open, onClose, onSave }) {
+    const [bill, setBill] = React.useState({
+        payeeName: "",
+        paymentDue: "",
+        paid: false
+    });
+
+    const handleChange = (e) => {
+        setBill({ ...bill, [e.target.name]: e.target.value });
+    };
+
+    const handleSave = () => {
+        onSave(bill);
+        onClose();
+    };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>{bill.billId > 0 ? "Edit Bill" : "New Bill"}</DialogTitle>
+      <DialogContent>
+        <TextField
+          margin="dense"
+          label="Payee Name"
+          name="payeeName"
+          fullWidth
+          value={bill.payeeName}
+          onChange={handleChange}
+        />
+        <TextField
+          margin="dense"
+          label="Payment Due"
+          name="paymentDue"
+          type="number"
+          fullWidth
+          value={bill.paymentDue}
+          onChange={handleChange}
+        />
+        <FormControlLabel
+          control={
+            <Checkbox
+            name="paid"
+            checked={bill.paid}
+            onChange={(e) =>
+              setBill({ ...bill, paid: e.target.checked })
+            }
+          />
+        }
+        label="Paid"
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button variant="contained" onClick={handleSave}>
+          Save
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
--- react-restclient/pages/bill/crud.js
+++ react-restclient/pages/bill/crud.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Button, Box } from "@mui/material";
-import BillTable from "@/components/BillTable";
-//import { useNavigate } from "react-router-dom";
 import Header from "@/components/Header";
 import NavBar from "@/components/NavBar";
+import BillDialog from "@/components/BillDialog";
+import BillTable from "@/components/BillTable";
 
 const TEST_BILLS = [
   { billId: 1, payeeName: 'Ali', paymentDue: 35.2, paid: false },
@@ -11,8 +11,8 @@ const TEST_BILLS = [
 ];
 
 export default function Home() {
+    const [open, setOpen] = useState(false);
     const [bills, setBills] = useState([]);
-    //const navigate = useNavigate();
 
     const loadData = () => {
         //getBills().then((res) => setBills(res.data));
@@ -22,6 +22,14 @@ export default function Home() {
     useEffect(() => {
         loadData();
     }, []);
+
+    const handleSaveBill = (bill) => {
+        console.log("Saved bill:", bill);
+        // TODO: Add / Update bill to backend
+
+        // Update in local
+        setBills([...bills, bill]);
+    };
 
     const handleDelete = async (id) => {
         await deleteBill(id);
@@ -36,10 +44,16 @@ export default function Home() {
 
             <Box sx={{ padding: 3 }}>
                 <Box sx={{ mb: 2 }}>
-                    <Button variant="contained" onClick={() => navigate("/bills/new")}>
+                    <Button variant="contained" onClick={() => setOpen(true)}>
                     New Bill
                     </Button>
                 </Box>
+
+                <BillDialog
+                    open={open}
+                    onClose={() => setOpen(false)}
+                    onSave={handleSaveBill}
+                />
                 <BillTable bills={bills} onDelete={handleDelete} />
             </Box>
         </div>

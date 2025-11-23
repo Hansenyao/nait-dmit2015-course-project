598ad148e4ca1aab7e6856a439dcc4ddf0e875b3
Youfang yao <yyfsingler@163.com>
2025-11-22 23:18:58 -0700

    add error message for dto field

--- cshap-restapi/ViewModels/BillDto.cs
+++ cshap-restapi/ViewModels/BillDto.cs
@@ -1,4 +1,5 @@
 ﻿using CShap.RestApi.Entities;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices;
 
 namespace CShap.RestApi.ViewModels
@@ -6,8 +7,11 @@ namespace CShap.RestApi.ViewModels
     public class BillDto
     {
         public int BillID { get; set; }
-        public string PayeeName { get; set; } = string.Empty;
-        public double PaymentDue {  get; set; }
+        [Required(ErrorMessage = "PayeeName is required")]
+        [MinLength(1, ErrorMessage = "PayeeName cannot be empty")]
+        public string PayeeName { get; set; } = default!;
+        [Range(0.01, double.MaxValue, ErrorMessage = "PaymentDue must be greater than 0")]
+        public double PaymentDue {  get; set; } = default!;
         public bool Paid { get; set; }
         public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }

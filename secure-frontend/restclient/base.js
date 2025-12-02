ceb2d9549e0bda5ed107d1060b7af37aa1dd91b5
Youfang yao <yyfsingler@163.com>
2025-12-02 14:56:26 -0700

    update the online REST API url

--- secure-frontend/restclient/base.js
+++ secure-frontend/restclient/base.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:5281/restapi/BillDtos"
+const BASE_URL = "https://course-project-restapi-gzhxgrctgcabfndu.canadacentral-01.azurewebsites.net/"
 
 export { BASE_URL }

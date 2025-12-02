4afc7b6c70f3e98ab8a8e286a9ba07f9cb11e3be
Youfang yao <yyfsingler@163.com>
2025-12-01 19:42:17 -0700

    add Clerk for login

--- secure-frontend/components/NavBar.js
+++ secure-frontend/components/NavBar.js
@@ -1,27 +1,54 @@
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-
-import AccountCircle from '@mui/icons-material/AccountCircle';
-
-import Link from 'next/link'
+import { AppBar, Toolbar, Typography, Box, Link, IconButton, Menu, MenuItem } from "@mui/material";
+import { UserButton, SignedIn, SignedOut, useUser } from "@clerk/nextjs";
 
 export default function NavBar() {
+  const { user } = useUser();
+  const role = user?.publicMetadata?.role ?? "User";
+  console.log("role: ", role);
+
   return <Box sx={{ flexGrow: 1 }}>
     <AppBar position="static">
       <Toolbar>
-        <Link href="/">
+        <Link href="/" style={{ textDecoration: "none", color: "inherit" }}>
           <Typography variant="h6" component="div">
             Home
           </Typography>
         </Link>
-        <Link href="/bill/crud">
+
+        <Link href="/bill/crud" style={{ textDecoration: "none", color: "inherit" }}>
           <Typography variant="h6" component="div" sx={{ ml: 3 }}>
             Bill CRUD
           </Typography>
         </Link>
+
+        <Box sx={{ flexGrow: 1 }} />
+
+        {/* Signed in：Display username + role */}
+        <SignedIn>
+          <Typography variant="body1" sx={{ mr: 2 }}>
+            {user?.username || user?.primaryEmailAddress?.emailAddress}
+            {" "}({role})
+          </Typography>
+
+          {/* Clerk Icon + menu */}
+          <UserButton afterSignOutUrl="/" />
+        </SignedIn>
+
+        {/* Non-signed in: Dispaly Login */}
+        <SignedOut>
+          <Link
+            href="/sign-in"
+            style={{
+              textDecoration: "none",
+              color: "inherit",
+              marginRight: "16px",
+            }}
+          >
+            <Typography variant="body1">
+              Login
+            </Typography>
+          </Link>
+        </SignedOut>
       </Toolbar>
     </AppBar>
   </Box>
